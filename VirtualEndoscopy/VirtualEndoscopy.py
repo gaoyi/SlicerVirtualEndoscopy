934234e6a03c2d16d3bce0bf9d00a80b002441e5
Yi Gao <gy99@tsinghua.org.cn>
2022-05-09 10:40:20 +0800

    STYLE: put this extension into the Endoscopy category

--- VirtualEndoscopy/VirtualEndoscopy.py
+++ VirtualEndoscopy/VirtualEndoscopy.py
@@ -22,7 +22,7 @@ class VirtualEndoscopy(ScriptedLoadableModule):
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
     self.parent.title = "Virtual Endoscopy"  # TODO: make this more human readable by adding spaces
-    self.parent.categories = ["VirtualEndoscopy"]  # TODO: set categories (folders where the module shows up in the module selector)
+    self.parent.categories = ["Endoscopy"]  # TODO: set categories (folders where the module shows up in the module selector)
     self.parent.dependencies = []  # TODO: add here list of module names that this module requires
     self.parent.contributors = ["Yi Gao(Shenzhen University), Haofan Huang (Shenzhen University)"]  # TODO: replace with "Firstname Lastname (Organization)"
     # TODO: update with short description of the module and a link to online module documentation
